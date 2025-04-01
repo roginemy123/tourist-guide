b142fecb4a0316971cc147c215da05bf3da32aba
bawiga <bawigarogine02@gmail.com>
2025-04-01 21:40:46 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cebu-tourist-guide/',
+  base: '/tourist-guide/',
   build: {
     outDir: 'docs'
   }
